0bb760f58ab6675332258abb6134a00477754e0a
Pedro Henrique <78716475+AdministratedAdmin@users.noreply.github.com>
2025-10-27 15:57:22 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+a = 6
+b = 4
+c = 4
+d = 2
+
+produto = (a * b) - (c * d)
+
+print("DIFERENÇA =", produto)
---
+++ compra.py
@@ -0,0 +1,9 @@
+precounitario = 12.40
+quantidade = 3
+total = 3 * 12.40
+
+print("Cadeira Infantil")
+print("Preço unitário: R$ 12.40")
+print("Quantidade: 3")
+
+print("Total da compra:", total)
